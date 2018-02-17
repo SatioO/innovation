1255076c3ba41bd4357caa19e11148d8fb2764d6
Brain <vaibhav.satam29991@gmail.com>
2018-02-17 19:51:53 +0530

    fix(forms): fixed async validation example

--- src/components/form/async-change/asyncValidate.js
+++ src/components/form/async-change/asyncValidate.js
@@ -4,8 +4,7 @@ const asyncValidate = (values /*, dispatch */) => {
 	return sleep(1000).then(() => {
 		// simulate server latency
 		if (["john", "paul", "george", "ringo"].includes(values.username)) {
-			/*eslint no-throw-literal: "error"*/
-			throw new Error({ username: "That username is taken" });
+			throw { username: "That username is taken" };
 		}
 	});
 };
